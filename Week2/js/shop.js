89b91cd1166653c7a7fc841f19936c398d582e0d
niteshchulani <nickchulani99@gmail.com>
2016-03-29 16:11:53 +0700

    week 2

---
+++ Week2/js/shop.js
@@ -0,0 +1,9 @@
+var customerName="Nitesh";
+var price= 10;
+var quantity= 2;
+var totalPrice= price * quantity;
+
+var customerElement = document.getElementById("customer-name");
+customerElement.textContent = customerName;
+var customerElement = document.getElementById("price");
+customerElement.textContent = totalPrice;
